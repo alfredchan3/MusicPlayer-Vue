b259ff447ab598d5b7b9ed9650938cda09b42246
Alfred Chen <brilychen@gmail.com>
2019-05-22 11:27:27 +0800

    git init commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
 
 Vue.config.productionTip = false
+
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+})
+
+fastclick.attach(document.body)
 
 new Vue({
   render: h => h(App),
